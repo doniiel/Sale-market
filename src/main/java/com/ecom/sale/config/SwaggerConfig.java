ad5bc30f919c9ae18b9c06aa2d6568b54192915a
Orynbek Daniyal <orynbekdanial8@gmail.com>
2025-08-23 18:35:07 +0500

    fix swagger error

--- src/main/java/com/ecom/sale/config/SwaggerConfig.java
+++ src/main/java/com/ecom/sale/config/SwaggerConfig.java
@@ -1,19 +1,30 @@
 package com.ecom.sale.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Sale Market API",
-                version = "1.0",
-                description = "Documentation REST API for sale market",
-                contact = @Contact(
-                        name = "Orynbek Daniyal",
-                        email = "orynbekdanial8@gmail.com"
-                )
-        )
-)
+import java.util.List;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sale Market API")
+                        .version("3.0.3")
+                        .description("Documentation REST API for sale market")
+                        .contact(new Contact()
+                                .name("Orynbek Daniyal")
+                                .email("orynbekdanial8@gmail.com")
+                        )
+                )
+                .servers(List.of(new Server().url("http://localhost:8080")));
+    }
+
 }
