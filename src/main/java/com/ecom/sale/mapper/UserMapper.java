c09c2fb3fb3a2fbeb816b49948d730f1859bebba
Orynbek Daniyal <orynbekdanial8@gmail.com>
2025-08-24 14:04:59 +0500

    fix error

--- src/main/java/com/ecom/sale/mapper/UserMapper.java
+++ src/main/java/com/ecom/sale/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.ecom.sale.mapper;
 
 import com.ecom.sale.dto.UserDto;
 import com.ecom.sale.model.User;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toDto(User user);
